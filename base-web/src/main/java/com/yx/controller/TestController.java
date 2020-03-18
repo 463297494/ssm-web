7ba0d4efa9148780bebbfd54717cdacc8106969d
root <463297494@qq.com>
2020-03-18 21:06:14 +0800

    555

--- base-web/src/main/java/com/yx/controller/TestController.java
+++ base-web/src/main/java/com/yx/controller/TestController.java
@@ -23,7 +23,7 @@ public class TestController {
 	public String test(int stuno, String stuname, int stuage) {
 		Student student = new Student(stuno, stuname, stuage);
 		studentService.addStudent(student);
-		return "测试输出成功123111!";
+		return "测试输出成功5555!";
 	}
 
 	@RequestMapping(value = "/user", method = RequestMethod.GET)
