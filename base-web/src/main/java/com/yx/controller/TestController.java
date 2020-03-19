3d36edadb4992a4fcf411a6ac38756c0ca60d17b
root <463297494@qq.com>
2020-03-19 08:04:52 +0800

    add

--- base-web/src/main/java/com/yx/controller/TestController.java
+++ base-web/src/main/java/com/yx/controller/TestController.java
@@ -23,7 +23,7 @@ public class TestController {
 	public String test(int stuno, String stuname, int stuage) {
 		Student student = new Student(stuno, stuname, stuage);
 		studentService.addStudent(student);
-		return "测试输出成功888!";
+		return "测试输出成功999!";
 	}
 
 	@RequestMapping(value = "/user", method = RequestMethod.GET)
